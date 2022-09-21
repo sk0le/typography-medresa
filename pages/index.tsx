67abce06bcca1808222857ddf254476822404618
Amel <you@example.com>
2022-09-21 11:29:12 +0200

    added all default components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import React, { useState } from "react";
+import { ChromePicker } from "react-color";
 
 const Home: NextPage = () => {
+  const [textColor, setTextColor] = useState("#ffffff");
+
+  const onChangeColor = (color: any, e: React.SyntheticEvent<any, any>) => {
+    setTextColor(color);
+  };
+
   return (
     <div
       style={{
@@ -13,9 +18,9 @@ const Home: NextPage = () => {
         backgroundImage:
           "linear-gradient(to bottom, rgba(0, 0, 0, 0.42), rgba(0, 0, 0, 0.63)), url(http://historija.info/wp-content/uploads/2019/03/Behram-begova-medresa3-e1552944182673.jpg)",
       }}
-      className="w-full min-h-screen h-full flex justify-center items-center"
+      className="w-full min-h-screen h-full flex flex-col-reverse xl:flex-row xl:justify-around items-center"
     >
-      <div className="max-w-lg w-full px-10 py-10 h-full bg-neutral-900/70 backdrop-blur-xl rounded-lg">
+      <div className="max-w-lg w-full px-10 py-10 h-full bg-neutral-900/60 backdrop-blur-xl rounded-lg">
         <h1 className="text-3xl text-neutral-100 font-medium mb-3">
           Promjena fonta:
         </h1>
@@ -39,7 +44,7 @@ const Home: NextPage = () => {
           className="w-full h-2 bg-gray-900/90 mt-2 rounded-lg appearance-none cursor-pointer"
         />
         <h1 className="text-3xl mt-6 text-neutral-100 font-medium mb-3">
-          Font Weight:
+          Debljina Fonta:
         </h1>
         <div className="flex mt-4">
           <button className="bg-neutral-900/70 backdrop-blur-lg w-full text-neutral-100 px-6 py-2 font-medium">
@@ -58,6 +63,17 @@ const Home: NextPage = () => {
             Black
           </button>
         </div>
+        <h1 className="text-3xl mt-6 text-neutral-100 font-medium mb-3">
+          Boja:
+        </h1>
+        <ChromePicker
+          className="mt-4"
+          color={textColor}
+          onChange={onChangeColor}
+        />
+      </div>
+      <div className="h-full py-6 px-12 bg-neutral-900/60 backdrop-blur-lg rounded-lg">
+        <h1 className="text-[10rem] text-neutral-100">Medresa</h1>
       </div>
     </div>
   );
